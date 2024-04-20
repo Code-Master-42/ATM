d8a8a520f75c01611435cd91ef65a3faa9ea34ff
Code-Master-42 <165811793+Code-Master-42@users.noreply.github.com>
2024-04-20 11:18:04 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let totalBalance = 20000;
+const pinNumber = 2999;
+let pinEntered = await inquirer.prompt([{
+        name: "pin",
+        message: "Enter your pin number",
+        type: "number",
+    }]);
+//console.log(pinEntered.pin)
+if (pinEntered.pin == pinNumber) {
+    let atmQuestions = await inquirer.prompt([{
+            name: "accountType",
+            message: "Select your account type",
+            type: "list",
+            choices: [
+                "Current Account",
+                "Savings Account",
+            ]
+        },
+        {
+            name: "transMethod",
+            message: "Select your transaction method",
+            type: "list",
+            choices: [
+                "Cash Withdrawal",
+                "Fast Cash"
+            ]
+        }
+    ]);
+    if (atmQuestions.transaction == "cash withdhrawal") {
+        let cashwithdrawAmount = await inquirer.prompt([{
+                name: "withdrawal",
+                message: "Enter the account you want to withdraw",
+                type: "number",
+            }]);
+        //Greater than or equal to operator
+        if (totalBalance >= cashwithdrawAmount.withdrawal) {
+            totalBalance == cashwithdrawAmount.withdrawal;
+            console.log(`Your Total Balance is:${totalBalance}`);
+        }
+        else
+            (console.log(`Insufficent Balance`));
+    }
+    else {
+        let fastCashAmount = await inquirer.prompt([{
+                name: "fastCash",
+                message: "Select the account youwant to withdraw",
+                type: "list",
+                choices: [
+                    "1000",
+                    "3000",
+                    "5000",
+                ]
+            }]);
+        if (totalBalance >= fastCashAmount.fastCash) {
+            totalBalance == fastCashAmount.fastCash;
+            console.log(`Your Total Balance is :${totalBalance}`);
+        }
+        else {
+            console.log(`Insufficient Balance`);
+        }
+    }
+}
